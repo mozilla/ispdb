a7fbb1c6b6349a3e4d2dc245c815162d1e0fe163
gozer@mozillamessaging.com <gozer@mozillamessaging.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-22 18:08:59 +0000

    import ispdb e916e6ccb87f
    git-svn-id: http://svn.mozilla.org/mozillamessaging.com/sites/ispdb.mozillamessaging.com/trunk@30241 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ settings.py
@@ -0,0 +1,100 @@
+# Django settings for ispdb project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'ispdb.sqlite'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = '/Users/davida/src/ispdb/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '02$7gj$t@dtd7um)d#3zlfg7ommedl#*ekt@@*ysy_fns(lp9+'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'ispdb.middleware.x_ssl',
+)
+
+ROOT_URLCONF = 'ispdb.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    "/Users/davida/src/ispdb/templates",
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.comments',
+    'django_openid_auth',
+    'ispdb.config'
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django_openid_auth.auth.OpenIDBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+OPENID_CREATE_USERS = True
+
+OPENID_UPDATE_DETAILS_FROM_SREG = True
+
+LOGIN_URL = '/openid/login'
+LOGIN_REDIRECT_URL = '/'
+DOCS_ROOT="/Users/davida/src/django/docs/"
+LOCAL_DEVELOPMENT = True
+USE_I18N = False
---
+++ urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls.defaults import *
+from ispdb.config.models import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+import settings
+
+unclaimed_dict = {
+    'queryset': UnclaimedDomain.objects.all(),
+}
+
+
+urlpatterns = patterns('',
+    (r'^admin/(.*)', admin.site.root),
+    (r'^comments/', include('django.contrib.comments.urls')),
+    (r'^openid/', include('django_openid_auth.urls')),
+    url(r'^logout/', 'ispdb.config.views.logout_view', name='ispdb_logout'),
+    url(r'^admin_login/', 'ispdb.config.views.admin_login', name="ispdb_login"),
+    url(r'^$', 'ispdb.config.views.intro', name='ispdb_index'),
+    url(r'^list', 'ispdb.config.views.list', name='ispdb_list'),
+    url(r'^details/(?P<id>\d*)', 'ispdb.config.views.details', name='ispdb_details'),
+    url(r'^export_xml/(?P<id>\d*)', 'ispdb.config.views.export_xml', name='ispdb_export_xml'),
+    url(r'^add/(?P<domain>.*)', 'ispdb.config.views.add', name='ispdb_add'),
+    url(r'^add/', 'ispdb.config.views.add', name='ispdb_add'),
+    url(r'^queue$', 'ispdb.config.views.queue', name='ispdb_queue'),
+    url(r'^policy$', 'ispdb.config.views.policy', name='ispdb_policy'),
+    url(r'^approve/(?P<id>\d*)', 'ispdb.config.views.approve', name='ispdb_approve'),
+)
+
+if settings.LOCAL_DEVELOPMENT:
+    urlpatterns += patterns("django.views",
+        url(r"%s(?P<path>.*)$" % settings.MEDIA_URL[1:], "static.serve", {
+            "document_root": settings.MEDIA_ROOT,
+        })
+    )
+
