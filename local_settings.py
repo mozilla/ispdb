689cc9e7418c2ebf3543d8b64cc3255843b1ea72
bwinton@latte.ca <bwinton@latte.ca@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-11-03 20:09:19 +0000

    Bug 518840 - Split out local settings into local_settings.py, and use os.path.abspath(__file__) instead of hardcoding davida's home directory.  p=ehenry5@gmail.com, r=bwinton,gozer
    git-svn-id: http://svn.mozilla.org/mozillamessaging.com/sites/ispdb.mozillamessaging.com/trunk@54978 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ local_settings.py
@@ -0,0 +1,34 @@
+import os
+from settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+LOCAL_DEVELOPMENT = True
+
+ISPDB_ROOT = os.path.dirname(__file__)
+
+#set the database storage location to right inside the project root
+DATABASE_NAME = os.path.join(ISPDB_ROOT, 'ispdb.sqlite')
+
+# Absolute path to the directory that holds media.
+MEDIA_ROOT = os.path.join(ISPDB_ROOT, 'media/')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a trailing slash.
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+LOGIN_URL = '/openid/login'
+LOGIN_REDIRECT_URL = '/'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ISPDB_ROOT, "templates/"),
+    os.path.join(ISPDB_ROOT, "templates/config/")
+)
+
--- settings.py
+++ settings.py
@@ -1,21 +1,17 @@
 import os
 # Django settings for ispdb project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'ispdb.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'         # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'ispdb.sqlite'      # Or path to database file if using sqlite3.
+DATABASE_USER = ''                  # Not used with sqlite3.
+DATABASE_PASSWORD = ''              # Not used with sqlite3.
+DATABASE_HOST = ''                  # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''                  # Set to empty string for default. Not used with sqlite3.
 
 
 #Test Runner
@@ -43,21 +39,7 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/Users/davida/src/ispdb/media/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin_media/'
+USE_I18N = False
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '02$7gj$t@dtd7um)d#3zlfg7ommedl#*ekt@@*ysy_fns(lp9+'
@@ -66,24 +48,15 @@ SECRET_KEY = '02$7gj$t@dtd7um)d#3zlfg7ommedl#*ekt@@*ysy_fns(lp9+'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#    'ispdb.middleware.x_ssl',
 )
 
 ROOT_URLCONF = 'ispdb.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "/Users/davida/src/ispdb/templates",
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -101,13 +74,3 @@ AUTHENTICATION_BACKENDS = (
     'django_openid_auth.auth.OpenIDBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
-
-OPENID_CREATE_USERS = True
-
-OPENID_UPDATE_DETAILS_FROM_SREG = True
-
-LOGIN_URL = '/openid/login'
-LOGIN_REDIRECT_URL = '/'
-DOCS_ROOT="/Users/davida/src/django/docs/"
-LOCAL_DEVELOPMENT = True
-USE_I18N = False
