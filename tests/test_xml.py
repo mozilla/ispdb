bed26e3035e37b4b56f2304566eb546d8c75d91d
Blake Winton <bwinton@latte.ca>
2009-12-06 00:09:36 +0000

    Bug 522253 - Validate the xml against a schema, and clean up the xml a little.  p=LouieDinh@gmail.com, r=bwinton,davida.
    git-svn-id: http://svn.mozilla.org/mozillamessaging.com/sites/ispdb.mozillamessaging.com/trunk@57387 4eb1ac78-321c-0410-a911-ec516a8615a5

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,57 +1,56 @@
 # -*- coding: utf-8 -*-
+import re
+import os
+from StringIO import StringIO
 
 from django.test import TestCase
-from ispdb.config import views,models
 from django.core.urlresolvers import reverse
 from django.test.client import Client
-import nose.tools
-import re
 
+from nose.tools import *
+import nose.tools
+from lxml import etree
+
+from ispdb.config import views,models
 
 class XMLTest(TestCase):
 
     fixtures = ['xml_testdata']
 
-    def test_export_xml(self):
-        reference_string = """<?xml version="1.0" encoding="UTF-8"?>
-        <clientConfig>
-          <emailProvider id="">
-          <id>1</id>
-          <lastUpdateDatetime>2009-09-30 00:24:54</lastUpdateDatetime>
-          <createdDatetime>2009-09-30 00:24:54</createdDatetime>
-          <approved>False</approved>
-          <invalid>False</invalid>
-          <emailProviderId></emailProviderId>
-          <displayName>NetZero Email</displayName>
-          <displayShortName>netzero</displayShortName>
-          <incomingServer type="imap">
-            <hostname>hostname_in</hostname>
-            <port>143</port>
-            <socketType>SSL</socketType>
-            <usernameForm></usernameForm>
-            <authentication>plain</authentication>
-            </incomingServer>
-          <outgoingServer>
-            <hostname>hostname_out</hostname>
-            <port>25</port>
-            <socketType>None</socketType>
-            <usernameForm>%EMAILLOCALPART%</usernameForm>
-            <authentication>plain</authentication>
-            <addThisServer>False</addThisServer>
-            <useGlobalPreferredServer>False</useGlobalPreferredServer>
-            </outgoingServer>
-          <settingsPageUrl>http://www.referencematerial.com/</settingsPageUrl>
-          <settingsPageLanguage>en</settingsPageLanguage>
-          <flaggedAsIncorrect>False</flaggedAsIncorrect>
-          <flaggedByEmail></flaggedByEmail>
-          <confirmations>0</confirmations>
-          <problems>0</problems>
-          </emailProvider>
-        </clientConfig>"""
-        reference_string = re.sub(r'\s','',reference_string)
-
+    def test_validated_export_xml(self):
         domain = models.Domain.objects.get(name="test.com")
         config_xml = domain.config.as_xml()
-        config_xml = re.sub(r'\s','',config_xml)
-        assert config_xml == reference_string
+        doc = etree.XML(config_xml)
+        xml_schema = etree.RelaxNG(file=os.path.join(os.getcwd(),
+                                                    'tests/relaxng_schema.xml'))
+        xml_schema.assertValid(doc)
+
+    def test_xml_content(self):
+        expected = { "domain" : ["test.com", "aim.com"],
+            "displayName" : "NetZero Email",
+            "displayShortName" : "netzero" }
+        expected_incoming = { "hostname" : "hostname_in", "port" : "143",
+            "socketType" : "SSL", "username" : None,
+            "authentication" : "plain", }
+        expected_outgoing = { "hostname" : "hostname_out",
+            "port" : "25", "socketType" : "SSL",
+            "username" : "%EMAILLOCALPART%", "authentication" : "plain",
+            "addThisServer" : "false", "useGlobalPreferredServer" : "false" }
+        domain = models.Domain.objects.get(name="test.com")
+        config_xml = domain.config.as_xml()
+        doc = etree.XML(config_xml)
+        #gets incoming/outgoing server element root
+        root = doc.getiterator();
+        incoming = doc.find("emailProvider/incomingServer")
+        outgoing = doc.find("emailProvider/outgoingServer")
+        for element in root:
+            if element.tag in expected:
+                if type(expected[element.tag]) == list:
+                    assert element.text in expected[element.tag]
+                else:
+                    assert_equal(element.text, expected[element.tag])
+        for element in incoming:
+            assert_equal(element.text, expected_incoming[element.tag])
+        for element in outgoing:
+            assert_equal(element.text, expected_outgoing[element.tag])
 
