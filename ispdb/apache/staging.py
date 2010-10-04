149adbf30b6b8c3846f6d72536b0c5fc05f54aa4
Philippe M. Chiasson <gozer@ectoplasm.org>
2010-10-04 20:45:20 +0000

    Bug 581976 - Move IPSDB to MySQL
    git-svn-id: http://svn.mozilla.org/mozillamessaging.com/sites/ispdb.mozillamessaging.com/trunk@75174 4eb1ac78-321c-0410-a911-ec516a8615a5

--- ispdb/apache/staging.py
+++ ispdb/apache/staging.py
@@ -15,12 +15,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 #DATABASE_NAME = "%s/ispdb.sqlite" % project
-DATABASE_NAME = '/var/ispdb/staging/ispdb.sqlite'
-DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_NAME = 'stage_ispdb'
+DATABASE_USER = 'stage_user'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_HOST = 'db-write'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
